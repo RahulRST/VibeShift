652da323538c8cb0cc941e6457ff731ae44d7d30
RahulRST <srahuljayaraj@gmail.com>
2024-04-04 21:29:59 +0530

    music play done

--- VibeShift/urls.py
+++ VibeShift/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from VibeShift import settings
+
 urlpatterns = [
   path('music/', include('player.urls')),  # Include app URLs with a prefix 'music/'
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- player/urls.py
+++ player/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from VibeShift import settings
 from . import views
-from django.conf.urls.static import static
 
 urlpatterns = [
   path('', views.song_list, name='song_list'),  # List all songs
   path('song/<int:pk>/', views.song_detail, name='song_detail'), 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
